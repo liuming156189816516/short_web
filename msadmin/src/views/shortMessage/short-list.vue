5ac15a2ed69543cde554595acb7a00f16be1ce4c
lucky842064 <156575319+lucky842064@users.noreply.github.com>
2024-12-25 01:37:34 +0800

    ...

--- msadmin/src/views/shortMessage/short-list.vue
+++ msadmin/src/views/shortMessage/short-list.vue
@@ -72,6 +72,7 @@
             <el-table-column prop="sucess_num" :label="$t('sys_s019')" minWidth="100">
                 <template slot-scope="scope">
                   {{ scope.row.sucess_num }} ({{ scope.row.sucess_rate*100 }}%)
+                  <!-- {{ scope.row.sucess_num }} ({{ Math.round(scope.row.sucess_rate * 100)}}%) -->
                 </template>
             </el-table-column>
             <el-table-column prop="fail_num" :label="$t('sys_s020')" minWidth="100" />
