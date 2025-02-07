b80bf6140a84d848445d125f6783e078418b0bf9
lucky842064 <156575319+lucky842064@users.noreply.github.com>
2025-02-07 17:25:08 +0100

    ...

--- msadmin/src/api/rcs.js
+++ msadmin/src/api/rcs.js
@@ -1,6 +1,14 @@
 /*** 通用配置*/
 import request from '@/utils/request'
-
+export const getchannellist = (params)=> {
+  return request({url:'/channel/getchannellist',method:'post',data:params})
+}
+export const changechannel = (params)=> {
+  return request({url:'/channel/changechannel',method:'post',data:params})
+}
+export const getuidchannel = (params)=> {
+  return request({url:'/channel/getuidchannel',method:'post',data:params})
+}
 export const getsmstasklist = (params)=> {
   return request({url:'/rcs/getrcstasklist',method:'post',data:params})
 }
@@ -15,12 +23,6 @@ export const getsmstaskinfolist = (params)=> {
 }
 export const delsmstasklist = (params)=> {
   return request({url:'/rcs/delrcstasklist',method:'post',data:params})
-}
-export const changechannel = (params)=> {
-  return request({url:'/channel/changechannel',method:'post',data:params})
-}
-export const getuidchannel = (params)=> {
-  return request({url:'/channel/getuidchannel',method:'post',data:params})
 }
 export const getsmstalklist = (params)=> {
   return request({url:'/rcs/getrcstalklist',method:'post',data:params})
